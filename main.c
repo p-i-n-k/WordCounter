bb05c04ff139662ae5b5825d9b84c85a52faed3a
Anton <stady.mail.itmo@gmail.com>
2021-09-15 15:32:40 +0300

    add command line args support

--- main.c
+++ main.c
@@ -17,13 +17,40 @@ int main(int argc, const char * argv[]) {
         perror("Enter file name");
         exit(0);
     }
-    FILE *file_ = fopen(argv[1], "r");
+    FILE *file_ = fopen(argv[argc-1], "r");
     if (file_ == NULL) {
         perror("file doesn't exist");
+        exit(0);
     }
     file_info info = get_file_info(file_);
-    
-    printf("\t%d %d %d %s\n", info.lines_num, info.words_num, info.size, argv[1]);
+
+    const char* short_options = "lwc";
+    const struct option long_options[] = {
+		{"lines", no_argument, NULL, 'l'},
+		{"words", optional_argument, NULL, 'w'},
+		{"bytes", required_argument, NULL, 'c'},
+		{NULL,0,NULL,0}
+	};
+
+    int res;
+    while ((res = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+        switch (res)
+        {
+        case 'w':
+            printf("words %d\n", info.words_num);
+            break;
+        case 'l':
+             printf("lines %d\n", info.lines_num);
+            break;
+        case 'c':
+             printf("bytes %d\n", info.size);
+            break;
+        default:
+            break;
+        }
+    }
+
+    //printf("\t%d %d %d %s\n", info.lines_num, info.words_num, info.size, argv[1]);
     
     
     
